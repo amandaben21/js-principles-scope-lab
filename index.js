9dd6fa18dde4ca33777a7958998401c5fbf8c64b
Amanda <amandaben21@aol.com>
2021-04-30 00:31:35 -0400

    Done

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function upperCaseCustomerName() { // modifies the customerName variable
 function setBestCustomer() {
     bestCustomer = 'not bob' // setBestCustomer
 }
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob' // overwrites the best customer
+}
+
+const leastFavoriteCustomer = 'is not defined' // declared const as a global scope now. 
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'is not defined' // unsuccessfully tries to reassign the least favorite customer
+}
