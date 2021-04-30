995d37b082360f0a6d4bab7acda09e692f04a941
Amanda <amandaben21@aol.com>
2021-04-30 00:22:11 -0400

    Was a bit confuse when it said set BestCustomer is not defined

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+
+var customerName = 'bob'; // declare customerName to be bob in global scope and returns the customerName
+
+function upperCaseCustomerName() { // modifies the customerName variable
+    customerName = 'BOB'
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob' // setBestCustomer
+}
